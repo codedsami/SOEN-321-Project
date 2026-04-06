c56679a52b509d964d561e9303b770d71828c21e
codedsami <miskat.mahmud@mail.concordia.ca>
2026-04-06 18:02:06 -0400

    db init

--- secure-file-sharing/backend/app.py
+++ secure-file-sharing/backend/app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from models import db
+from auth import auth_bp
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///secure_files.db'
+
+db.init_app(app)
+app.register_blueprint(auth_bp)
 
 @app.route('/')
 def index():
     return {'status': 'backend is running'}
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
